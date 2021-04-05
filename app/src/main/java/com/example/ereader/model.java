9c065bd66300695f269d1d07ef5aca31a0f79fa3
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2021-04-05 18:21:27 +0530

    Retrieving PDF from database and displaying functionality completed

---
+++ app/src/main/java/com/example/ereader/model.java
@@ -0,0 +1,59 @@
+package com.example.ereader;
+
+public class model {
+    String filename, fileurl;
+    int nod, nol, nov;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFileurl() {
+        return fileurl;
+    }
+
+    public void setFileurl(String fileurl) {
+        this.fileurl = fileurl;
+    }
+
+    public int getNod() {
+        return nod;
+    }
+
+    public void setNod(int nod) {
+        this.nod = nod;
+    }
+
+    public int getNol() {
+        return nol;
+    }
+
+    public void setNol(int nol) {
+        this.nol = nol;
+    }
+
+    public int getNov() {
+        return nov;
+    }
+
+    public void setNov(int nov) {
+        this.nov = nov;
+    }
+
+    public model(String filename, String fileurl, int nod, int nol, int nov) {
+        this.filename = filename;
+        this.fileurl = fileurl;
+        this.nod = nod;
+        this.nol = nol;
+        this.nov = nov;
+    }
+
+    public model() {
+    }
+
+
+}
